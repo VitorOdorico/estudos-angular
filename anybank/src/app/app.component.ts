d783e6e5366941838dd40de04d8416ab769ff730
VitorOdorico <ve743137@gmail.com>
2025-12-12 11:06:54 -0300

    forms

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
+import { ContaComponent } from "./banner/conta/conta.component";
+import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent],
+  imports: [
+    BannerComponent,
+    FormNovaTransacaoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
