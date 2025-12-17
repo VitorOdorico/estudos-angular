93570021d46372ab9802658e4b47a18b3de4dd00
VitorOdorico <ve743137@gmail.com>
2025-12-17 10:29:51 -0300

    feat extratos

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
-import { ContaComponent } from "./banner/conta/conta.component";
 import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
+import { TipoTransacao, Transacao } from './modelos/transacao';
+import { ExtratoComponent } from "./extrato/extrato.component";
 
 @Component({
   selector: 'app-root',
-  imports: [
-    BannerComponent,
-    FormNovaTransacaoComponent
-  ],
+  imports: [BannerComponent, FormNovaTransacaoComponent, ExtratoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'anybank';
+  transacoes = signal<Transacao[]>([]);
+
+  saldo = computed(() => {
+    return this.transacoes().reduce((acc, transacaoAtual) => {
+      switch (transacaoAtual.tipo) {
+        case TipoTransacao.DEPOSITO:
+          return acc + transacaoAtual.valor;
+
+        case TipoTransacao.SAQUE:
+          return acc - transacaoAtual.valor;
+
+        default:
+          throw new Error('Tipo de transação não identificado.');
+      }
+    }, 0);
+  });
+
+  processarTransacao(transacao: Transacao) {
+    if (transacao.tipo === TipoTransacao.SAQUE && transacao.valor > this.saldo()) {
+      return alert('Saldo insuficiente!');
+    }
+
+    this.transacoes.update((listaAtual) => [transacao, ...listaAtual]);
+  }
 }
--- anybank/src/app/form-nova-transacao/form-nova-transacao.component.ts
+++ anybank/src/app/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TipoTransacao, Transacao } from '../modelos/transacao';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [FormsModule],
+  imports: [FormsModule, KeyValuePipe],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
 export class FormNovaTransacaoComponent {
-  valorTransacao = "40";
+  valorTransacao = "";
+  tipoTransacao = "";
+  transacaoCriada = output <Transacao>();
+  tipoTransacaoEnum = TipoTransacao;
+
   aoSubmeter(){
-    console.log(this.valorTransacao)
+    const transacao = new Transacao(
+      this.tipoTransacao as TipoTransacao,
+      Number(this.valorTransacao)
+    );
+    this.transacaoCriada.emit(transacao);
+      this.tipoTransacao = ""
+      this.valorTransacao = ""
+
+
   }
 }
